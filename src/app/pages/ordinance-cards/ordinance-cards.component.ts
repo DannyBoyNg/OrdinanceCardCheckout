d30a11de682342b422485977e9dec30ba20aba73
Danny Ng <dannyboyng@gmail.com>
2025-02-11 15:28:14 +0100

    changes

---
+++ src/app/pages/ordinance-cards/ordinance-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ordinance-cards',
+  imports: [],
+  templateUrl: './ordinance-cards.component.html',
+  styleUrl: './ordinance-cards.component.css'
+})
+export class OrdinanceCardsComponent {
+
+}
